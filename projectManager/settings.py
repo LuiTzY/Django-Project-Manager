0a335f6c352308df13c8fb78292f74da1234275a
Luis Marte <luisdavidmarte00@gmail.com>
2024-04-14 22:56:53 -0400

    whitenoise

--- projectManager/settings.py
+++ projectManager/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'whitenoise.runserver_nostatic',
     'users',
     'proyects',
     'events'
@@ -45,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -105,7 +107,9 @@ AUTH_USER_MODEL = "users.User"
 LOGIN_URL = '/singin/'
 
 ALLOWED_HOSTS = ['localhost','django-project-manager-production.up.railway.app']
-SESSION_COOKIE_SECURE = False
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
