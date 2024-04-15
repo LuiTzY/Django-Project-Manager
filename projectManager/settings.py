8bf8c1d6b1fbc9d1c4ed332cea587a8a581101bf
Luis Marte <luisdavidmarte00@gmail.com>
2024-04-14 22:33:27 -0400

    second commit

--- projectManager/settings.py
+++ projectManager/settings.py
@@ -76,18 +76,12 @@ WSGI_APPLICATION = 'projectManager.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME':'gestion_proyectos',
-        'USER':'root',
-        'PASSWORD':'admin',
-        'HOST':'localhost',
-        'PORT':3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
