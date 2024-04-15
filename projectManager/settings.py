8c100b40d7647478f088d1e3d678b2feaa4da396
Luis Marte <luisdavidmarte00@gmail.com>
2024-04-14 23:26:01 -0400

    postgresql and others

--- projectManager/settings.py
+++ projectManager/settings.py
@@ -12,7 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,10 +82,7 @@ WSGI_APPLICATION = 'projectManager.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # Password validation
@@ -109,6 +109,7 @@ LOGIN_URL = '/singin/'
 ALLOWED_HOSTS = ['localhost','django-project-manager-production.up.railway.app']
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+CSRF_TRUSTED_ORIGINS =['https://django-project-manager-production.up.railway.app']
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
